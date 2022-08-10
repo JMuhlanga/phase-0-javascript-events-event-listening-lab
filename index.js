b8519b4ab066e731de07ed7767d2767346866fc0
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-11 00:02:29 +0300

    Attempted on Aug 10

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let input;
+
+input = document.getElementById('button');
+
 function addingEventListener() {
+    alert(`I was Clicked!`);
 }
+
+input.addEventListener('click', addingEventListener);
